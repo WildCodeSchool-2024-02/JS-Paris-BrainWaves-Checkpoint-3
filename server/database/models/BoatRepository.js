d36d5415069f2a85f8f3091646f2028f34a608c7
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-06-10 13:00:12 +0200

    bonus not finished

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -5,17 +5,19 @@ class BoatRepository extends AbstractRepository {
     super({ table: "boat" });
   }
 
-  async readAll() {
+  async readAll(query) {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select boat.*, tile.type, tile.has_treasure from ${this.table} as boat join tile on boat.coord_x=tile.coord_x and boat.coord_y=tile.coord_y`);
-
-    // Return the array of boats
-    return rows;
+    if (query.name) {
+      const [rows] = await this.database.query(`select boat.*, tile.type, tile.has_treasure from ${this.table} as boat join tile on boat.coord_x=tile.coord_x and boat.coord_y=tile.coord_y where boat.name = ?`, [query.name]);
+      // Return the array of boats
+      return rows;
+    }
+      const [boats] = await this.database.query(`select * from ${this.table}`);
+      return boats;
   }
 
   async update(boat) {
     const {id, coord_x, coord_y} = boat;
-    console.info(boat);
     const [updatedBoat] = await this.database.query(`update ${this.table} set coord_x = ?, coord_y = ? where id = ?`, [coord_x, coord_y, id]);
     return updatedBoat.affectedRows;
   }
