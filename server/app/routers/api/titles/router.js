ffd8ed380a02f21070d6082e59bb54de6df0ae38
Erwan Bataille <erwan.bataille2910@gmail.com>
2024-06-10 10:40:00 +0200

    step 2

--- server/app/routers/api/titles/router.js
+++ server/app/routers/api/titles/router.js
@@ -1,19 +1,14 @@
 const express = require("express");
 
 const router = express.Router();
+
 /* ************************************************************************* */
-// Import And Use Routers Here
+// Define Your API Routes Here
 /* ************************************************************************* */
-const boatsRouter = require("./boats/router");
 
-router.use("/boats", boatsRouter);
-const gamesRouter = require("./games/router");
+const { browse } = require("../../../controllers/tileActions");
 
-router.use("/games", gamesRouter);
-
-const tilesRouter = require("./tiles/router");
-
-router.use("/tiles", tilesRouter);
+router.get("/", browse);
 
 /* ************************************************************************* */
 
