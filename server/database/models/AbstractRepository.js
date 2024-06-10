88d1b611d0adb5154d0cc530c29928c2154368f2
Antoine <ant.delalande@gmail.com>
2024-06-10 12:16:24 +0200

    finished step 5

--- server/database/models/AbstractRepository.js
+++ server/database/models/AbstractRepository.js
@@ -3,7 +3,7 @@ const database = require("../client");
 
 // Provide database access through AbstractRepository class
 class AbstractRepository {
-  constructor({ table }) {
+  constructor({ table, table2 }) {
     // thx https://www.codeheroes.fr/2017/11/08/js-classes-abstraites-et-interfaces/
     if (this.constructor === AbstractRepository) {
       throw new TypeError(
@@ -13,6 +13,7 @@ class AbstractRepository {
 
     // Store the table name
     this.table = table;
+    this.table2 = table2;
 
     // Provide access to the database client
     this.database = database;
--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -1,20 +1,32 @@
 const AbstractRepository = require("./AbstractRepository");
 
+
 class BoatRepository extends AbstractRepository {
   constructor() {
-    super({ table: "boat" });
+    super({ table: "boat", table2: "tile" });
   }
 
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+    const b = this.table
+    const t = this.table2
+    const [rows] = await this.database.query(
+      `select ${b}.*, ${t}.type, ${t}.has_treasure 
+      from ${b} 
+      join ${t} 
+      on ${t}.coord_x = ${b}.coord_x 
+      and ${t}.coord_y = ${b}.coord_y`
+    );
 
     // Return the array of boats
     return rows;
   }
 
   async update(boat) {
-    const [move] = await this.database.query(`UPDATE ${this.table} SET ? WHERE id = ?`, [boat, boat.id])
+    const [move] = await this.database.query(
+      `UPDATE ${this.table} SET ? WHERE id = ?`,
+      [boat, boat.id]
+    );
     return move.affectedRows;
   }
 }
