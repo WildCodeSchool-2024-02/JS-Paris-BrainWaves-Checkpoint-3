0ebfd3574067d9af03e3828eb173c80e2b962622
Erwan Bataille <erwan.bataille2910@gmail.com>
2024-06-10 10:14:10 +0200

    step 1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -5,11 +5,11 @@ CREATE TABLE boat (
   coord_y INT NOT NULL,
   PRIMARY KEY(id)
 );
-
 CREATE TABLE tile (
   id INT AUTO_INCREMENT NOT NULL,
   type VARCHAR(255) NOT NULL,
   coord_x INT NOT NULL,
   coord_y INT NOT NULL,
+  has_treasure BOOLEAN NOT NULL DEFAULT 0,
   PRIMARY KEY(id)
 );
