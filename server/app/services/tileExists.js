6cf1c3ab4cf91f97ab75369297569fbda93ec176
Pcsrio <airyoldi@gmail.com>
2024-06-10 13:54:51 +0200

    step4

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,18 @@
-module.exports = null;
+const tables = require("../../database/tables");
+
+const valideTitle = async (req, res, next) => {
+    try {
+        const [result] = await tables.tile.readByCoordinates(req.body.coord_x, req.body.coord_y);
+        if (result){
+            next()
+        }
+        else {
+            res.sendStatus(422)
+        }
+    } catch (err){
+        next(err)
+    }
+   
+}
+
+module.exports = valideTitle
