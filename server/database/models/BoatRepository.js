2a8e712fcbd5ad35f0259fa1d47146000137979d
Pcsrio <airyoldi@gmail.com>
2024-06-10 14:12:01 +0200

    step5

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -7,10 +7,20 @@ class BoatRepository extends AbstractRepository {
 
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+    const [rows] = await this.database.query(
+      `select * from ${this.table} JOIN tile ON boat.id = tile.id`
+    );
 
     // Return the array of boats
     return rows;
+  }
+
+  async update(boat) {
+    const [row] = await this.database.query(
+      `update ${this.table} set coord_x= ?, coord_y=? WHERE id = ? `,
+      [boat.coord_x, boat.coord_y, boat.id]
+    );
+    return row.affectedRows;
   }
 }
 
