98b9d0c00897cdcc61aa276838fe5ae9172fea6c
maximmft <mmaufront@gmail.com>
2024-06-10 10:01:20 +0200

    step2 done

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,17 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const tiles = await tables.tile.readAll();
+
+    // Respond with the boats in JSON format
+    res.json(tiles);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+module.exports = {
+  browse,
+};
