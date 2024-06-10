4708a45ca358351d6eb6450caf93ab1d11e10f5e
vvdam <maaalouladam@gmail.com>
2024-06-10 10:33:58 +0200

    finish step2

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,17 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    // Fetch all boats from the database
+    const tile = await tables.tile.readAll();
+    // Respond with the boats in JSON format
+    res.status(200).json(tile);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+module.exports = {
+  browse,
+};
