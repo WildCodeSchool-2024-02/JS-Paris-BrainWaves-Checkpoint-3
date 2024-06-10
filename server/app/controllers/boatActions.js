3747c1f6dc27d75a466a8e0fb9f282adc2838d91
maximmft <mmaufront@gmail.com>
2024-06-10 11:27:19 +0200

    step3 done

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,21 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req,res,next) => {
+  try {
+    const boat = req.body;
+    const result = await tables.boat.update(boat)
+    if (result)
+      res.sendStatus(204);
+    else 
+      res.sendStatus(404);
+    
+  } catch (err){
+    next(err);
+  }
+}
+
 module.exports = {
   browse,
+  edit,
 };
--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -12,6 +12,18 @@ class BoatRepository extends AbstractRepository {
     // Return the array of boats
     return rows;
   }
+
+  async update(boat) {
+    const { coord_x, coord_y, id } = boat;
+    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
+    const [rows] = await this.database.query(
+      `update boat set coord_x = ?, coord_y = ? where id = ?`,
+      [coord_x, coord_y, id]
+    );
+
+    // Return the array of boats
+    return rows.affectedRows;
+  }
 }
 
 module.exports = BoatRepository;
