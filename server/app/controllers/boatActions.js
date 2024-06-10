33035d2e35ce5ea0122b8270d00119f01c320341
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2024-06-10 13:11:08 +0200

    step bonus

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -3,12 +3,9 @@ const tables = require("../../database/tables");
 const browse = async (req, res, next) => {
   try {
     // Fetch all boats from the database
-    const boats = await tables.boat.readAll();
-
-    // Respond with the boats in JSON format
-    res.json(boats);
+      const boatQuery = await tables.boat.readAll(req.query);
+      res.json(boatQuery)
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -5,12 +5,15 @@ class BoatRepository extends AbstractRepository {
     super({ table: "boat" });
   }
 
-  async readAll() {
+  async readAll(where) {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select boat.*, tile.type, tile.has_treasure from ${this.table} as boat join tile on boat.coord_x = tile.coord_x and boat.coord_y = tile.coord_y `);
-
-    // Return the array of boats
-    return rows;
+    if (where == null){
+      const [rowsQuery ] = await this.database.query(`select boat.*, tile.type, tile.has_treasure from ${this.table} as boat join tile on boat.coord_x = tile.coord_x and boat.coord_y = tile.coord_y`)
+      return rowsQuery
+    }
+    const [rows] = await this.database.query(`select boat.*, tile.type, tile.has_treasure from ${this.table} as boat join tile on boat.coord_x = tile.coord_x and boat.coord_y = tile.coord_y where boat.name = ?`,
+    [where.name]);
+    return rows
   }
 
   async update(boat){
