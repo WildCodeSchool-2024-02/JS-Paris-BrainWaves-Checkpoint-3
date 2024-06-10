56e46f0365e8f0e37f99fe2b80792d2132af94a5
YannLahellec <lahellec.yann@gmail.com>
2024-06-10 12:35:24 +0200

    set4 done

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,12 @@
-module.exports = null;
+
+const validateTiles = (req,res,next) => {
+    const {coord_x, coord_y} = req.body;
+    if(coord_x === 0 && coord_x <= 11 && coord_y === 0 && coord_y <= 5) {
+      return next()
+    } else {
+    return res.sendStatus(422)
+    }
+}
+
+
+module.exports = validateTiles
